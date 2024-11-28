c9e3d19ab33197a60d29095e3593c7e43ee7b27e
Surya <155997081+surya-d-naidu@users.noreply.github.com>
2024-11-28 22:06:55 +0530

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -33,7 +33,7 @@ exports.register = async (req, res) => {
 
     
     const verificationUrl = `flashroad.vercel.app/api/auth/verify-email/${emailVerificationToken}`;
-    const mailContent = `Please verify your email by clicking the following link: ${verificationUrl}`;
+    const mailContent = `Here is where you should do : ${verificationUrl}`;
     sendEmail(email, 'Verify Your Email', mailContent);
 
     res.status(201).json({ message: 'User registered successfully, please check your email for verification.' });
