e67490282d60458e6227dc249739d6f0a3c172b0
Surya <155997081+surya-d-naidu@users.noreply.github.com>
2024-11-29 06:15:49 +0530

    Update emailService.js

--- server/services/emailService.js
+++ server/services/emailService.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',  
+  host: 'smtp.gmail.com',
   port: 465,         
   auth: {
     user: process.env.EMAIL_USER,  
