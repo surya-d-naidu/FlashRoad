e1b0f380c279548506173e877270e44c30f66c8d
Surya <155997081+surya-d-naidu@users.noreply.github.com>
2024-11-29 05:53:51 +0530

    Update emailService.js

--- server/services/emailService.js
+++ server/services/emailService.js
@@ -1,33 +1,40 @@
 const nodemailer = require('nodemailer');
 const dotenv = require('dotenv');
 
+
 dotenv.config();
 
+
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  port: 465,
+  service: 'gmail',  
+  port: 465,         
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    user: process.env.EMAIL_USER,  
+    pass: process.env.EMAIL_PASS,  
   },
+  secure: true,  
 });
+
 
 const sendEmail = async (to, subject, text) => {
   const mailOptions = {
-    from: process.env.EMAIL_USER,
-    to,
-    subject,
-    text,
+    from: process.env.EMAIL_USER,  
+    to,                           
+    subject,                      
+    text,                         
   };
 
   try {
+    
     const info = await transporter.sendMail(mailOptions);
     console.log('Email sent:', info.response);
-    return info; // You can return the result here if needed.
+    return info;  
   } catch (error) {
+    
     console.error('Error sending email:', error);
-    throw error; // Rethrow the error if you need to catch it higher up.
+    throw error;  
   }
 };
+
 
 module.exports = sendEmail;
