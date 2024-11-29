109c860023a6c36d858d6ac9ab121c2028e9f56a
Surya <155997081+surya-d-naidu@users.noreply.github.com>
2024-11-29 07:12:05 +0530

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -12,8 +12,7 @@ exports.register = async (req, res) => {
     const userRole = role.toLowerCase();
     const userExists = await User.findOne({ email });
     if (userExists){  
-      return res.render('400').json({ message: 'User already exists' 
-    }
+      return res.status('400').json({ message: 'User already exists' }
   )} ;
 
     
